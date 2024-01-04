42512d3c09caa7a8be14ff6bdd04c142d0d39b0b
husham35 <husham35@gmail.com>
2024-01-04 13:20:38 -0800

    3. Infinite addition corrected

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,10 @@ if __name__ == "__main__":
 
     num_args = sys.argv
 
+    total = 0
+
     if len(num_args) > 1:
-        total = 0
         for i in range(len(num_args) - 1):
             total += int(num_args[i + 1])
 
-        print("{}".format(total))
+    print("{}".format(total))
