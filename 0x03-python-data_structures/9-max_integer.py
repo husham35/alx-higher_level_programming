90ee26a3620cd981f1c2e169434c12cd225c8650
husham35 <husham35@gmail.com>
2024-01-08 11:38:15 -0800

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return 'None'
+    else:
+        max_num = my_list[0]
+        for i in range(1, len(my_list)):
+            if max_num < my_list[i]:
+                max_num = my_list[i]
+            else:
+                continue
+        return max_num
