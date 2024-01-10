9c23f922147fed06792d1dd2b304b795abd685ac
husham35 <husham35@gmail.com>
2024-01-10 10:45:13 -0800

    roman nums corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
 
-    numerals = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
-    }
+    nums = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            }
 
-    if any(c not in numerals for c in roman_string):
+    if any(c not in nums for c in roman_string):
         return 0
 
-    rom_num = numerals[roman_string[0]]
-
+    r_num = nums[roman_string[0]]
     for i, s in enumerate(roman_string[1:], 1):
-        if numerals[roman_string[i - 1]] < numerals[roman_string[i]]:
-            rom_num += numerals[roman_string[i]] - 2 * (numerals[roman_string[i - 1]])
+        if nums[roman_string[i-1]] < nums[roman_string[i]]:
+            r_num += nums[roman_string[i]] - 2*(nums[roman_string[i-1]])
         else:
-            rom_num += numerals[s]
+            r_num += nums[s]
 
-    return rom_num
+    return r_num
