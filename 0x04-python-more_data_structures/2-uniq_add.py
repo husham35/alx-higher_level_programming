d4a06881f9eb5a85bf79b98da50b9b6e4009abd9
husham35 <husham35@gmail.com>
2024-01-10 09:04:15 -0800

    corrected 2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,8 +4,5 @@ def uniq_add(my_list=[]):
     if not my_list:
         return my_list
 
-    unique_items =  set()
-    for item in my_list:
-        if isinstance(item, int):
-            unique_items.add(item)
+    unique_items =  set(my_list)
     return sum(unique_items)
