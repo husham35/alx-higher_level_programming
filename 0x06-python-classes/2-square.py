a5706483a30eb118cf810072dcbac53fc2a41f97
husham35 <husham35@gmail.com>
2024-01-23 22:24:43 +0000

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,8 +13,10 @@ class Square:
         Initialization of instance of attribute size to 0
         Args: size (int): size of square
         """
-        if type(size) is not int:
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
             raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >=0')
-        self.__size = size
