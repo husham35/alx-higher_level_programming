9cee00fbffb9f758a2a382e79284a60c5c04fd70
husham35 <husham35@gmail.com>
2024-02-06 18:02:28 +0000

    corrected task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,7 @@ Module that adds items to a list
 import os
 import sys
 
-save_to_file = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
@@ -18,4 +18,4 @@ if __name__ == "__main__":
         from_file = []
 
     from_file.extend(args)
-    save_to_file(from_file, filename)
+    save_to_json_file(from_file, filename)
