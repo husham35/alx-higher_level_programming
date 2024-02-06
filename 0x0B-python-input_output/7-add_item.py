3079d6f339095cf52ae6512ea877dccb211a4e17
husham35 <husham35@gmail.com>
2024-02-06 18:05:24 +0000

    corrected task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """
 Module that adds items to a list
 """
-import os
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -10,12 +9,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
     filename = "add-item.json"
-    args = list(sys.argv[1:])
-
-    if os.path.isfile(filename):
-        from_file = list(load_from_json_file(filename))
-    else:
-        from_file = []
-
-    from_file.extend(args)
-    save_to_json_file(from_file, filename)
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
