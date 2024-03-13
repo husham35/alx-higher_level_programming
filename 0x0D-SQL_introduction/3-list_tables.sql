d505dbf38edffd274abf5efdd0176a5100a74838
husham35 <husham35@gmail.com>
2024-03-13 05:07:39 +0000

    corrected tasks 1 to 10

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,5 @@
-/*
-A script that lists all the tables of a database in your MySQL server.
-The database name will be passed as argument of mysql command
-(in the following example: mysql is the name of the database)
-*/
+-- A script that lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command
+-- (in the following example: mysql is the name of the database)
+
 SHOW TABLES;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,4 @@
-/*
-A script that displays the number of records with id = 89 in the
-table `first_table` of the database `hbtn_0c_0` in your MySQL server.
-*/
+-- A script that displays the number of records with id = 89 in the
+-- table `first_table` of the database `hbtn_0c_0` in your MySQL server.
+
 SELECE COUNT(*) FROM first_table WHERE id = 89;
