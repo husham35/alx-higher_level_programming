dbc0785d6c289b56bccd0868e7b8fbe3dce7a48f
husham35 <husham35@gmail.com>
2024-04-27 02:35:13 +0000

    task 0 fixes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,13 +7,13 @@ Python script that fetches `https://alx-intranet.hbtn.io/status`
     the following example (tabulation before -)
   * You must use a `with` statement
 """
-import urllib.request as req
+import urllib.request
 
 if __name__ == "__main__":
-    _req = req.Request("https://alx-intranet.hbtn.io/status")
-    with req.urlopen(_req) as response:
-        html_body = response.read()
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
         print("Body response:")
-        print(f"\t- type: {type(html_body)}")
-        print(f"\t- content: {html_body}")
-        print(f"\t-  utf8 content: {html_body.decode('utf-8')}")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
