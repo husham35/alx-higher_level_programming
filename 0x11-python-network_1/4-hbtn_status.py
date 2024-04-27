16cc20f3c5ce4548b6f1398f5587cc4c9cb2f215
husham35 <husham35@gmail.com>
2024-04-27 02:53:29 +0000

    task 4 fixes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,7 +9,7 @@ Python script that fetches `https://alx-intranet.hbtn.io/status`
 import requests
 
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print(f"\t- type: {type(response.text)}")
