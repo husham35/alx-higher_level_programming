0f8455cd907a03c269ee71259ad50124720a7931
husham35 <husham35@gmail.com>
2024-04-27 02:37:49 +0000

    task 0 fixes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,5 +12,5 @@ import requests
 if __name__ == "__main_":
     response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
