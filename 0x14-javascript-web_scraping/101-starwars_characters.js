e7cd075ec87527b2f33467c4e6503fe773c8b4f4
husham35 <husham35@gmail.com>
2024-05-22 04:23:27 +0000

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,3 +3,25 @@
 
 const request = require('request');
 
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+// get movie ID
+const movieId = process.argv[2];
+
+request(apiUrl + movieId, function (err, response, body) {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+// function to recursively print individual character
+function printCharacters (characters, idx) {
+  request(characters[idx], function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
+    }
+  });
+}
